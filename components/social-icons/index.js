f6269f00fc27b72a1482e85679feba8ca5932b4c
Darian Nocera <darnocer@gmail.com>
2023-08-07 20:10:51 -0600

    Update social icon size

--- components/social-icons/index.js
+++ components/social-icons/index.js
@@ -31,7 +31,7 @@ const SocialIcon = ({ kind, href, size = 8 }) => {
     >
       <span className="sr-only">{kind}</span>
       <SocialSvg
-        className={`fill-current text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 h-${size} w-${size}`}
+        className={`h-6 fill-current text-gray-700 duration-200 ease-in hover:text-yellow-500 dark:text-gray-200 dark:hover:text-blue-400`}
       />
     </a>
   )
