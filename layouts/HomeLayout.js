1f459122458a4c88294d8f63bc3061472fee1434
Darian Nocera <darnocer@gmail.com>
2023-08-07 20:13:33 -0600

    Update HomeLayout: - Add SectionContainer - Styling

--- layouts/HomeLayout.js
+++ layouts/HomeLayout.js
@@ -1,15 +1,14 @@
-import SocialIcon from '@/components/social-icons'
-import Image from '@/components/Image'
-import { PageSEO } from '@/components/SEO'
 import siteMetadata from '@/data/siteMetadata'
+import SectionContainer from '@/components/SectionContainer'
 
 export default function HomeLayout({ children, frontMatter }) {
   return (
-    <>
-      <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-        {siteMetadata.title}
-      </h1>
-      <div className="prose max-w-none space-y-2 pt-6 pb-8 md:space-y-5">{children}</div>
-    </>
+    <SectionContainer>
+      <div className="border-b-2 border-gray-300">
+        <div className="max-w-full space-y-2 pt-6 pb-8 dark:text-gray-300 md:space-y-5">
+          {children}
+        </div>
+      </div>
+    </SectionContainer>
   )
 }
