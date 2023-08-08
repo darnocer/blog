2045719846a7c781cd977ea4739b2fc47e23c79e
Darian Nocera <darnocer@gmail.com>
2023-08-07 20:17:56 -0600

    404 Content and styling changes

--- pages/404.js
+++ pages/404.js
@@ -14,13 +14,10 @@ export default function FourZeroFour() {
         </div>
         <div className="max-w-md">
           <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
-            Sorry we couldn't find this page.
-          </p>
-          <p className="mb-8">
-            But dont worry, you can find plenty of other things on our homepage.
+            Oops! Not sure where that page went...
           </p>
           <Link href="/">
-            <button className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500">
+            <button className="focus:shadow-outline-primary inline rounded-lg border border-transparent bg-primary-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 duration-200 ease-in hover:bg-primary-700 focus:outline-none dark:hover:bg-primary-500">
               Back to homepage
             </button>
           </Link>
