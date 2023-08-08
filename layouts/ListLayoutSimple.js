56ce86f47b28b3dcb5dd5befb03633f66d2c8179
Darian Nocera <darnocer@gmail.com>
2023-08-07 20:15:53 -0600

    Updates to ListLayoutSimple: - Update layout - Make entire line link - Add date - Add | - Add LinkArrow - Add SectionContainer - Update Max Display

--- layouts/ListLayoutSimple.js
+++ layouts/ListLayoutSimple.js
@@ -4,54 +4,50 @@ import siteMetadata from '@/data/siteMetadata'
 import { useState } from 'react'
 // import { getAllFilesFrontMatter } from '@/lib/mdx'
 import Pagination from '@/components/Pagination'
-import formatDate from '@/lib/utils/formatDate'
+import formatDateShort from '@/lib/utils/formatDateShort'
+import SectionContainer from '@/components/SectionContainer'
+import LinkArrow from '@/components/LinkArrow'
 
-const MAX_DISPLAY = 10
+const MAX_DISPLAY = 5
 
 export default function ListLayoutSimple({ posts }) {
   return (
-    <>
-      <ul className="divide-y divide-gray-200 dark:divide-gray-700">
-        {!posts.length && 'No posts found.'}
-        {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
-          const { slug, date, title, summary, tags } = frontMatter
-          return (
-            <li key={slug} className="py-4">
-              <article>
-                <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                  <div className="space-y-5 xl:col-span-3">
-                    <div className="space-y-6">
-                      <div>
-                        <h3 className="text-xl font-bold leading-8 tracking-tight">
-                          <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
-                            {title}
-                          </Link>
-                        </h3>
-                        <div className="flex flex-wrap">
+    <SectionContainer>
+      <div className="border-b-1 border-gray-300 py-4">
+        <h2 className="text-4xl font-extrabold">Recent Posts</h2>
+      </div>
+      <div className="space-y-6">
+        <ul className="space-y-6">
+          {!posts.length && 'No posts found.'}
+          {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
+            const { slug, date, title, summary, tags } = frontMatter
+            return (
+              <li key={slug} className="py-0">
+                <article className="justify-stretch flex flex-row flex-nowrap items-center items-baseline gap-x-2">
+                  <Link
+                    href={`/blog/${slug}`}
+                    className="justify-stretch flex content-center items-baseline gap-x-4"
+                  >
+                    <time className="w-14 text-gray-500 dark:text-gray-300" dateTime={date}>
+                      {formatDateShort(date)}
+                    </time>
+                    <span className="text-gray-400 dark:text-gray-400">|</span>
+                    <h3 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-gray-100">
+                      {title}
+                    </h3>
+                    {/* <div className="flex flex-wrap">
                           {tags.map((tag) => (
                             <Tag key={tag} text={tag} />
                           ))}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </article>
-            </li>
-          )
-        })}
-      </ul>
-      {posts.length > MAX_DISPLAY && (
-        <div className="flex justify-start text-base font-medium leading-6">
-          <Link
-            href="/blog"
-            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-            aria-label="all posts"
-          >
-            View All &rarr;
-          </Link>
-        </div>
-      )}
-    </>
+                        </div> */}
+                  </Link>
+                </article>
+              </li>
+            )
+          })}
+        </ul>
+        {posts.length > MAX_DISPLAY && <LinkArrow text="View All" />}
+      </div>
+    </SectionContainer>
   )
 }
