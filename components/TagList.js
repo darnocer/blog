b80f4be2de948a0f42545bab861704699f18ed95
Darian Nocera <darnocer@gmail.com>
2023-08-07 20:09:22 -0600

    Update Tag List layout and add SectionContainer

--- components/TagList.js
+++ components/TagList.js
@@ -1,15 +1,16 @@
 import Link from '@/components/Link'
 import Tag from '@/components/Tag'
 import kebabCase from '@/lib/utils/kebabCase'
+import SectionContainer from './SectionContainer'
 
 export default function Tags({ tags }) {
   const sortedTags = Object.keys(tags).sort((a, b) => tags[b] - tags[a])
   return (
-    <>
+    <SectionContainer>
       <div className="border-b-1 border-gray-300 py-2">
         <h2 className="text-4xl font-extrabold">Topics</h2>
       </div>
-      <div className="flex flex-wrap">
+      <div className="flex flex-wrap justify-between">
         {Object.keys(tags).length === 0 && 'No tags found.'}
         {sortedTags.map((t) => {
           return (
@@ -25,6 +26,6 @@ export default function Tags({ tags }) {
           )
         })}
       </div>
-    </>
+    </SectionContainer>
   )
 }
