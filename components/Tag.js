0493cee0b6c8cd6385310a0f253f56e0821dcb97
Darian Nocera <darnocer@gmail.com>
2023-08-07 19:51:32 -0600

    Tag List Implementation on the homepage

--- components/Tag.js
+++ components/Tag.js
@@ -4,7 +4,7 @@ import kebabCase from '@/lib/utils/kebabCase'
 const Tag = ({ text }) => {
   return (
     <Link href={`/tags/${kebabCase(text)}`}>
-      <a className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+      <a className="mr-3 rounded-md bg-primary-500 bg-opacity-50 px-2 py-1 text-sm text-xs font-medium font-semibold uppercase uppercase text-primary-500 text-primary-900 duration-100 ease-in hover:bg-opacity-80 hover:text-gray-100 dark:text-primary-100 dark:hover:text-gray-100">
         {text.split(' ').join('-')}
       </a>
     </Link>
---
+++ components/TagList.js
@@ -0,0 +1,30 @@
+import Link from '@/components/Link'
+import Tag from '@/components/Tag'
+import kebabCase from '@/lib/utils/kebabCase'
+
+export default function Tags({ tags }) {
+  const sortedTags = Object.keys(tags).sort((a, b) => tags[b] - tags[a])
+  return (
+    <>
+      <div className="border-b-1 border-gray-300 py-2">
+        <h2 className="text-4xl font-extrabold">Topics</h2>
+      </div>
+      <div className="flex flex-wrap">
+        {Object.keys(tags).length === 0 && 'No tags found.'}
+        {sortedTags.map((t) => {
+          return (
+            <div key={t} className="mt-2 mb-2 mr-5">
+              <Tag text={t} />
+              {/* <Link
+                href={`/tags/${kebabCase(t)}`}
+                className="-ml-2 text-sm font-semibold uppercase text-gray-600 dark:text-gray-300"
+              >
+                {` (${tags[t]})`}
+              </Link> */}
+            </div>
+          )
+        })}
+      </div>
+    </>
+  )
+}
