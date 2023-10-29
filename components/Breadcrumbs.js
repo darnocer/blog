7a8a59418a7c3908e490b0dd9b3ca10e7a7a3118
Darian Nocera <darnocer@gmail.com>
2023-10-29 17:05:12 -0600

    determine directory for breadcrumbs

--- components/Breadcrumbs.js
+++ components/Breadcrumbs.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Link from '@/components/Link'
 import LinkArrow from './LinkArrow'
+import { useRouter } from 'next/router'
 
 export default function Breadcrumbs() {
+  const router = useRouter()
+  const isTipsPage = router.pathname.startsWith('/tips')
+
   return (
-    <Link className="flex text-primary-500 underline" href="/blog">
+    <Link className="mb-6 flex text-primary-500 underline" href={isTipsPage ? '/tips' : '/blog'}>
       <LinkArrow direction="left" /> Back
     </Link>
   )
