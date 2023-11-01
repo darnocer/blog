94f9b3e5e961a41d075dbdf98cab9745642a0f75
Darian Nocera <darnocer@gmail.com>
2023-11-01 09:41:12 -0600

    update pull script to copy new assets to public

--- pull.sh
+++ pull.sh
@@ -1,5 +1,12 @@
-# this script both pulls changes from the remote blog repo as well as any content updates from the content submodule 
-
 #!/bin/bash
+
+# Pull changes from the remote blog repo
 git pull origin master
+
+# Update the content submodule
 git submodule update --init --recursive
+
+# Copy only new image assets from /content/assets to /public/static/images/tips/
+rsync -av --ignore-existing content/assets/ public/static/images/tips/
+
+echo "Update complete!"
