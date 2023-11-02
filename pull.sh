8f23d93ef017084973cbd51ca9db9c976633cebe
Darian Nocera <darnocer@gmail.com>
2023-11-02 07:57:56 -0600

    update script to copy post images to public

--- pull.sh
+++ pull.sh
@@ -9,4 +9,7 @@ git submodule update --init --recursive
 # Copy only new image assets from /content/assets to /public/static/images/tips/
 rsync -av --ignore-existing content/assets/ public/static/images/tips/
 
+# Copy only new image assets from /content/assets/posts to /public/static/images/posts/
+rsync -av --ignore-existing content/assets/posts/ public/static/images/posts/
+
 echo "Update complete!"
