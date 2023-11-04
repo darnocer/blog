fe61097a93fc962498339719a5ae774851bd7d3a
Darian Nocera <darnocer@gmail.com>
2023-11-04 09:20:58 -0600

    update image styling

--- components/Image.js
+++ components/Image.js
@@ -1,6 +1,7 @@
-import NextImage from 'next/image'
-
-// eslint-disable-next-line jsx-a11y/alt-text
-const Image = ({ ...rest }) => <NextImage {...rest} />
+const Image = ({ src, alt, ...rest }) => (
+  <div className="flex justify-center">
+    <img src={src} alt={alt || 'image'} {...rest} className="mx-auto rounded-md" />{' '}
+  </div>
+)
 
 export default Image
