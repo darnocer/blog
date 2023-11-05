75e268408e6509f89e4cca413dd1082cd7881a36
Darian Nocera <darnocer@gmail.com>
2023-11-05 14:57:21 -0700

    update asset script directories: - only assets WITHIN the posts will be maintained in the content repo - the asset folder structure in obsidian now matches relative to public

--- pull.sh
+++ pull.sh
@@ -6,8 +6,8 @@ git pull origin master
 # Update the content submodule
 git submodule update --init --recursive
 
-# Copy only new image assets from /content/assets to /public/static/images/tips/
-rsync -av --ignore-existing content/assets/ public/static/images/tips/
+# Copy only new image assets from content/static/images/posts to public/static/images/posts/
+rsync -av --ignore-existing content/static/images/posts public/static/images/posts/
 
 
 echo "Update complete!"
