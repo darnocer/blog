50b827211dc60277e5e7200c309e3abc0cf20bda
Darian Nocera <darnocer@gmail.com>
2023-11-06 11:01:01 -0700

    rename update script

--- update.sh
+++ update.sh
@@ -3,11 +3,11 @@
 # Pull changes from the remote blog repo
 git pull origin master
 
-# Update the content submodule
-git submodule update --init --recursive
-
 # Copy only new image assets from content/static/images/posts to public/static/images/posts/
 rsync -av --ignore-existing content/static/images/posts public/static/images
+
+# sync to Airtable
+npm run sync
 
 
 echo "Update complete!"
