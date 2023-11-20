ffacae4c68e409a3ddf18de2e47030e21b482a7b
Darian Nocera <darnocer@gmail.com>
2023-11-20 11:21:52 -0700

    add margin to images

--- components/Image.js
+++ components/Image.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 
 const CustomImage = ({ src, alt, ...rest }) => (
-  <div className="flex justify-center">
+  <div className="my-4 flex justify-center">
     <Image src={src} alt={alt || 'image'} {...rest} className="mx-auto rounded-md" />
   </div>
 )
