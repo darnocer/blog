7d89847fe00b3fda0e08db2ee258ac03a576d772
Darian Nocera <darnocer@gmail.com>
2023-11-22 10:52:36 -0700

    create PostHeader component for posts

---
+++ components/PostHeader.js
@@ -0,0 +1,24 @@
+import PageTitle from './PageTitle'
+import HeaderTagList from './HeaderTagList'
+import formatDate from '@/lib/utils/formatDate'
+
+export default function PostHeader({ title, summary, tags, date }) {
+  return (
+    <div className="space-y-2 border-b border-gray-200 pb-8 text-center dark:border-gray-700">
+      <HeaderTagList tags={tags} />
+
+      <PageTitle>{title}</PageTitle>
+      <dl>
+        <div>
+          <dt className="sr-only">Published on</dt>
+          <dd className="text-xs font-medium uppercase leading-6 text-gray-500 dark:text-gray-400">
+            <time dateTime={date}>{formatDate(date)}</time>
+          </dd>
+        </div>
+      </dl>
+      {/* <div className="m-4">
+        <p className="italic">{summary}</p>
+      </div> */}
+    </div>
+  )
+}
--- layouts/PostSimple.js
+++ layouts/PostSimple.js
@@ -1,14 +1,13 @@
-import PageTitle from '@/components/PageTitle'
 import SectionContainer from '@/components/SectionContainer'
 import { BlogSEO } from '@/components/SEO'
 import siteMetadata from '@/data/siteMetadata'
-import formatDate from '@/lib/utils/formatDate'
+
 import Comments from '@/components/comments'
 import ScrollTopAndComment from '@/components/ScrollTopAndComment'
 import Breadcrumbs from '@/components/Breadcrumbs'
 import { useRouter } from 'next/router'
 import LinkArrow from '@/components/LinkArrow'
-import HeaderTagList from '@/components/HeaderTagList'
+import PostHeader from '@/components/PostHeader'
 
 import { useEffect } from 'react'
 
@@ -16,7 +15,7 @@ import Prism from 'prismjs'
 import '../lib/prism/dataview'
 
 export default function PostLayout({ frontMatter, authorDetails, next, prev, children }) {
-  const { date, title, tags } = frontMatter
+  const { date, title, tags, summary } = frontMatter
 
   const router = useRouter()
   const directory = router.pathname.split('/')[1]
@@ -33,19 +32,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
         <div>
           <header>
             <Breadcrumbs />
-            <div className="space-y-1 border-b border-gray-200 pb-10 text-center dark:border-gray-700">
-              {/* <dl>
-                <div>
-                  <dt className="sr-only">Published on</dt>
-                  <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                    <time dateTime={date}>{formatDate(date)}</time>
-                  </dd>
-                </div>
-              </dl> */}
-
-              <PageTitle>{title}</PageTitle>
-              <HeaderTagList tags={tags} />
-            </div>
+            <PostHeader title={title} summary={summary} tags={tags} date={date} />
           </header>
           <div
             className="divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:divide-y-0 "
