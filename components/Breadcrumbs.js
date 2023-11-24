afda88d9fcd3e752e8abcff7b34a86a509b3f240
Darian Nocera <darnocer@gmail.com>
2023-11-24 12:15:57 -0700

    implement conditional breadcrumb text

--- components/Breadcrumbs.js
+++ components/Breadcrumbs.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Link from '@/components/Link'
 import LinkArrow from './LinkArrow'
 import { useRouter } from 'next/router'
 
@@ -7,6 +6,7 @@ export default function Breadcrumbs() {
   const router = useRouter()
 
   const directory = router.pathname.split('/')[1]
-
-  return <LinkArrow text="Back" direction="left" href={`/${directory}`} />
+  const text = directory === 'blog' ? 'All Posts' : 'All Tips'
+  const href = directory === 'blog' ? '/posts' : '/tips'
+  return <LinkArrow text={text} direction="left" href={href} />
 }
