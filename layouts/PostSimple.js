6e334f0bea46786e33847aec85d6e8df6e9e53f3
Darian Nocera <darnocer@gmail.com>
2023-11-24 12:20:59 -0700

    update post layout container size

--- layouts/PostSimple.js
+++ layouts/PostSimple.js
@@ -25,7 +25,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
   }, [])
 
   return (
-    <SectionContainer>
+    <SectionContainer container="small">
       <BlogSEO url={`${siteMetadata.siteUrl}/blog/${frontMatter.slug}`} {...frontMatter} />
       <ScrollTopAndComment />
       <article>
@@ -39,7 +39,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
             style={{ gridTemplateRows: 'auto 1fr' }}
           >
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
-              <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">{children}</div>
+              <div className="section prose max-w-none pt-10 pb-8 dark:prose-dark">{children}</div>
             </div>
             <Comments frontMatter={frontMatter} />
             <footer>
