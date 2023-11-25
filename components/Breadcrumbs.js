44198f42992d85b62af1f3fd1bd269985b6e54bc
Darian Nocera <darnocer@gmail.com>
2023-11-24 17:34:58 -0700

    update breadcrumbs link

--- components/Breadcrumbs.js
+++ components/Breadcrumbs.js
@@ -6,7 +6,7 @@ export default function Breadcrumbs() {
   const router = useRouter()
 
   const directory = router.pathname.split('/')[1]
-  const text = directory === 'blog' ? 'All Posts' : 'All Tips'
-  const href = directory === 'blog' ? '/posts' : '/tips'
+  const text = directory === 'blog' ? 'Back to Blog' : 'Back to Tips'
+  const href = directory === 'blog' ? '/blog' : '/tips'
   return <LinkArrow text={text} direction="left" href={href} />
 }
