c47cbdaa2a2b937c84612eacdc7bbc9d5650b53a
Darian Nocera <darnocer@gmail.com>
2023-11-25 10:33:33 -0700

    add max-width-none to ContentLayout: - prose applies a max-width by default. this resolves the content taking up only 2/3 of the container

--- layouts/ContentLayout.js
+++ layouts/ContentLayout.js
@@ -1,3 +1,3 @@
 export default function ContentLayout({ children }) {
-  return <div className="prose dark:prose-dark">{children}</div>
+  return <div className="prose max-w-none leading-normal dark:prose-dark">{children}</div>
 }
