4d75cbed69bbe6177a447293b02c9fe0adcbeb11
Darian Nocera <darnocer@gmail.com>
2023-11-25 17:46:26 -0700

    update image border radius

--- components/Image.js
+++ components/Image.js
@@ -1,8 +1,15 @@
 import Image from 'next/image'
 
-const CustomImage = ({ src, alt, ...rest }) => (
+const CustomImage = ({ src, alt, borderRadius = 'rounded-md', width, height, ...rest }) => (
   <div className="my-4 flex justify-center">
-    <Image src={src} alt={alt || 'image'} {...rest} className="mx-auto rounded-md" />
+    <Image
+      className={borderRadius}
+      src={src}
+      alt={alt || 'image'}
+      width={width}
+      height={height}
+      {...rest}
+    />
   </div>
 )
 
