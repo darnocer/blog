29f90e31078089f8528517a603107fe94195fe67
Darian Nocera <darnocer@gmail.com>
2023-11-26 09:52:12 -0700

    add color prop for heading

--- components/Heading.js
+++ components/Heading.js
@@ -1,19 +1,21 @@
 import React from 'react'
 
-const HeadingLevels = {
-  h1: ({ text }) => <h1 className="text-6xl font-extrabold">{text}</h1>,
-  h2: ({ text }) => <h2 className="text-4xl font-extrabold">{text}</h2>,
-  h3: ({ text }) => <h3 className="text-2xl font-extrabold">{text}</h3>,
-}
-
-export default function Heading({ text, level = 'h2', border = 'border' }) {
-  const HeadingTag = HeadingLevels[level] || HeadingLevels.h2
+export default function Heading({ text, level = 'h2', border = 'border', color = 'gray-200' }) {
   const borderClasses =
     border === 'border' ? 'mb-4 border-b-1 border-gray-200 py-2 dark:border-gray-700' : ''
+  const textColor = `text-${color}`
+
+  const HeadingLevels = {
+    h1: ({ text }) => <h1 className="text-6xl font-extrabold">{text}</h1>,
+    h2: ({ text }) => <h2 className="text-4xl font-extrabold">{text}</h2>,
+    h3: ({ text }) => <h3 className={`text-2xl font-extrabold ${textColor}`}>{text}</h3>,
+  }
+
+  const HeadingTag = HeadingLevels[level] || HeadingLevels.h2
 
   return text ? (
     <div className={`${borderClasses}`}>
-      <HeadingTag text={text} />
+      <HeadingTag className={textColor} text={text} />
     </div>
   ) : null
 }
--- components/TagList.js
+++ components/TagList.js
@@ -5,11 +5,17 @@ import kebabCase from '@/lib/utils/kebabCase'
 import Heading from '@/components/Heading'
 import SectionContainer from './SectionContainer'
 
-export default function TagList({ tags, heading, level = 'h2', border = 'border' }) {
+export default function TagList({
+  tags,
+  heading,
+  level = 'h2',
+  border = 'border',
+  color = 'gray-200',
+}) {
   const sortedTags = Object.keys(tags).sort((a, b) => tags[b] - tags[a])
   return (
     <>
-      {heading ? <Heading text={heading} level={level} border={border} /> : null}
+      {heading ? <Heading text={heading} level={level} border={border} color={color} /> : null}
       <div className="mb-6 flex flex-wrap justify-start">
         {Object.keys(tags).length === 0 && 'No tags found.'}
         {sortedTags.map((t) => {
