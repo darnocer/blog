4d7ed9d00297d7171f129fa9c1aca16deba50fa7
Darian Nocera <darnocer@gmail.com>
2024-01-01 08:40:22 -0700

    tag style adjustments

--- components/Tag.js
+++ components/Tag.js
@@ -4,7 +4,7 @@ import kebabCase from '@/lib/utils/kebabCase'
 const Tag = ({ text, index }) => {
   return (
     <Link href={`/tags/${kebabCase(text)}`}>
-      <a className=" mr-3 rounded-md bg-primary-500 bg-opacity-50 px-2 py-1 text-xxs font-semibold uppercase uppercase leading-none tracking-wide text-primary-900  duration-100 ease-in hover:bg-opacity-60 hover:text-gray-800 dark:font-medium dark:text-primary-100 dark:hover:text-gray-100">
+      <a className=" mr-3 rounded-full bg-primary-500 bg-opacity-50 px-2.5 py-1 text-xxs font-semibold uppercase uppercase leading-none tracking-wide text-primary-900  duration-100 ease-in hover:bg-opacity-60 hover:text-gray-800 dark:font-medium dark:text-primary-100 dark:hover:text-gray-100">
         {text.split(' ').join('-')}
       </a>
     </Link>
