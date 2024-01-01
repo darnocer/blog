14dca94c27801ef4cad10e602b3c1073ee2e5d34
Darian Nocera <darnocer@gmail.com>
2024-01-01 08:40:11 -0700

    font size style adjustments

--- layouts/ContentLayout.js
+++ layouts/ContentLayout.js
@@ -1,3 +1,3 @@
 export default function ContentLayout({ children }) {
-  return <div className="prose max-w-none leading-normal dark:prose-dark">{children}</div>
+  return <div className="prose max-w-none text-lg leading-normal dark:prose-dark">{children}</div>
 }
