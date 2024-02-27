40f46b7462d4086902fb089d278384b43ed6491f
Darian Nocera <darnocer@gmail.com>
2024-02-27 14:40:43 -0700

    add filters to recentposts component

--- layouts/RecentPosts.js
+++ layouts/RecentPosts.js
@@ -1,7 +1,5 @@
 import Link from '@/components/Link'
-
 import formatDate from '@/lib/utils/formatDate'
-
 import LinkArrow from '@/components/LinkArrow'
 import Heading from '@/components/Heading'
 import SectionContainer from '@/components/SectionContainer'
@@ -10,36 +8,54 @@ import Badge from '@/components/Badge'
 
 const MAX_DISPLAY = 5
 
-export default function RecentPosts({ posts, heading, directory }) {
+export default function RecentPosts({
+  posts,
+  heading,
+  directory,
+  categoryFilter,
+  typeFilter,
+  tagFilter,
+}) {
+  // Adjusted to handle typeFilter as an array
+  const filteredByType = typeFilter
+    ? posts.filter((post) => post.content_type.some((type) => typeFilter.includes(type)))
+    : posts
+
+  const filteredByCategory = categoryFilter
+    ? filteredByType.filter((post) => post.category.some((cat) => categoryFilter.includes(cat)))
+    : filteredByType
+
+  const filteredByTags = tagFilter
+    ? filteredByCategory.filter((post) => {
+        if (Array.isArray(tagFilter)) {
+          return tagFilter.some((tag) => post.tags.includes(tag))
+        } else {
+          return post.tags.includes(tagFilter)
+        }
+      })
+    : filteredByCategory
+
   return (
-    <SectionContainer>
+    <SectionContainer padding="xlarge">
       <Heading text={heading} />
       <div className="space-y-6">
         <ul className="space-y-6">
-          {!posts.length && 'No posts found.'}
-          {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
-            const { slug, date, title, summary, tags, type, content_type } = frontMatter
+          {filteredByTags.length === 0 && 'No posts found.'}
+          {filteredByTags.slice(0, MAX_DISPLAY).map((frontMatter) => {
+            const { slug, date, title, summary, tags, content_type } = frontMatter
             return (
               <li key={slug} className="group no-arrow py-2">
                 <article className="flex flex-col">
-                  <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
-                    {/* <div className="flex items-baseline gap-x-2">
-                      <time
-                        className="text-xs uppercase text-gray-500 dark:text-gray-400"
-                        dateTime={date}
-                      >
-                        {formatDate(date)}
-                      </time>
-                    </div> */}
+                  <Link href={`/${slug}`} className="text-gray-900 dark:text-gray-100">
                     <div className="flex items-baseline gap-x-2 pt-1 pb-2 align-middle">
-                      <h3 className="text-2xl font-bold leading-snug tracking-tight text-gray-800 underline duration-200 ease-in hover:text-primary-600 dark:text-gray-200 dark:hover:text-primary-400">
+                      <h3 className="text-2xl font-extrabold leading-snug tracking-tight text-gray-800 duration-300 ease-in hover:underline dark:text-gray-200">
                         {title}
                       </h3>
-                      <Badge text={content_type} />
+                      <Badge text={content_type.join(', ')} />
                     </div>
                     <div className="flex items-baseline gap-x-2">
-                      {tags.map((tag, index) => (
-                        <Tag text={tag} />
+                      {tags.map((tag) => (
+                        <Tag key={tag} text={tag} />
                       ))}
                     </div>
                   </Link>
@@ -49,8 +65,8 @@ export default function RecentPosts({ posts, heading, directory }) {
           })}
         </ul>
         <div className="mt-2">
-          {posts.length > MAX_DISPLAY && (
-            <LinkArrow text="View All" direction="right" href={`/${directory}`} />
+          {filteredByTags.length > MAX_DISPLAY && (
+            <LinkArrow text="View All" direction="right" href={`/posts`} />
           )}
         </div>
       </div>
