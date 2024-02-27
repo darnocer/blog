1d4d16a66add45cc30ab4f8ee0e9174d31b27ad0
Darian Nocera <darnocer@gmail.com>
2024-02-26 20:39:32 -0700

    badge and tag styles

--- components/Badge.js
+++ components/Badge.js
@@ -1,9 +1,16 @@
 const Badge = ({ text }) => {
-  return (
-    <p className="mr-3 rounded-full bg-secondary-500 bg-opacity-60 px-2 py-1 text-xs font-semibold uppercase tracking-wide text-gray-900  duration-100 ease-in dark:font-medium dark:text-gray-100 ">
-      {text}
+  const badgeText = {
+    tips: 'tip',
+    musings: 'musing',
+    snippets: 'snippet',
+    guides: 'guide',
+    default: 'musing',
+  }
+  return text ? (
+    <p className="mr-3 rounded-2xl bg-secondary-500 bg-opacity-60 px-2 py-0.5 text-xs font-semibold uppercase tracking-wide text-gray-900  duration-100 ease-in dark:font-medium dark:text-gray-100 ">
+      {badgeText[text]}
     </p>
-  )
+  ) : null
 }
 
 export default Badge
--- layouts/RecentPosts.js
+++ layouts/RecentPosts.js
@@ -6,6 +6,7 @@ import LinkArrow from '@/components/LinkArrow'
 import Heading from '@/components/Heading'
 import SectionContainer from '@/components/SectionContainer'
 import Tag from '@/components/Tag'
+import Badge from '@/components/Badge'
 
 const MAX_DISPLAY = 5
 
@@ -17,22 +18,28 @@ export default function RecentPosts({ posts, heading, directory }) {
         <ul className="space-y-6">
           {!posts.length && 'No posts found.'}
           {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
-            const { slug, date, title, summary, tags, type } = frontMatter
+            const { slug, date, title, summary, tags, type, content_type } = frontMatter
             return (
               <li key={slug} className="group no-arrow py-2">
                 <article className="flex flex-col">
                   <Link href={`/${type}/${slug}`} className="text-gray-900 dark:text-gray-100">
-                    <h3 className="text-3xl font-bold leading-snug tracking-tight text-gray-800 underline duration-200 ease-in hover:text-primary-600 dark:text-gray-200 dark:hover:text-primary-400">
-                      {title}
-                    </h3>
-                    <div className="flex items-baseline gap-x-2">
-                      <time className="text-base text-gray-500 dark:text-gray-400" dateTime={date}>
+                    {/* <div className="flex items-baseline gap-x-2">
+                      <time
+                        className="text-xs uppercase text-gray-500 dark:text-gray-400"
+                        dateTime={date}
+                      >
                         {formatDate(date)}
                       </time>
+                    </div> */}
+                    <div className="flex items-baseline gap-x-2 pt-1 pb-2 align-middle">
+                      <h3 className="text-2xl font-bold leading-snug tracking-tight text-gray-800 underline duration-200 ease-in hover:text-primary-600 dark:text-gray-200 dark:hover:text-primary-400">
+                        {title}
+                      </h3>
+                      <Badge text={content_type} />
+                    </div>
+                    <div className="flex items-baseline gap-x-2">
                       {tags.map((tag, index) => (
-                        <div key={index} className="mt-2">
-                          <Tag text={tag} />
-                        </div>
+                        <Tag text={tag} />
                       ))}
                     </div>
                   </Link>
