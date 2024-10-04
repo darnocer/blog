8e1a697aa5fd94132a135c887a9ccc41333eea67
Darian Nocera <darnocer@gmail.com>
2024-10-04 08:58:32 -0600

    update badge style and fix alignment

--- components/Badge.js
+++ components/Badge.js
@@ -1,11 +1,7 @@
-import getBadgeText from '../lib/utils/getBadgeText'
-
 const Badge = ({ text }) => {
-  const badgeText = getBadgeText(text)
-
   return text ? (
-    <p className="mr-3 inline-flex rounded-2xl bg-secondary-400 bg-opacity-80 px-2 py-0.5 align-bottom text-xs font-semibold uppercase tracking-wide text-gray-800 duration-300 ease-in hover:bg-secondary-500 hover:bg-opacity-80 dark:bg-secondary-400  dark:bg-opacity-60 dark:text-gray-100 dark:hover:bg-secondary-300 dark:hover:bg-opacity-60">
-      {badgeText}
+    <p className="mr-3 inline-flex min-h-max min-w-max items-center rounded-2xl bg-secondary-400 bg-opacity-80 px-2 pt-1 text-xs font-semibold uppercase leading-normal tracking-wide text-gray-700 duration-300 ease-in hover:bg-secondary-500 hover:bg-opacity-80  dark:bg-secondary-400 dark:bg-opacity-60 dark:text-gray-100 dark:hover:bg-secondary-300 dark:hover:bg-opacity-60">
+      {text}
     </p>
   ) : null
 }
