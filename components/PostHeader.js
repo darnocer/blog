0be1f870136096664bb4c5f5ef23fe03dad3a267
Darian Nocera <darnocer@gmail.com>
2024-10-04 09:01:01 -0600

    update post header badge and date styles

--- components/PostHeader.js
+++ components/PostHeader.js
@@ -7,10 +7,10 @@ export default function PostHeader({ title, summary, tags, date, contentType }) 
   return (
     <div className="space-y-1 border-b border-gray-200 pb-8 text-center dark:border-gray-700">
       {date ? (
-        <dl>
+        <dl className="pb-4">
           <div>
             <dt className="sr-only">Published on</dt>
-            <dd className="text-xs font-medium uppercase leading-6 text-gray-500 dark:text-gray-400">
+            <dd className="text-xs font-medium uppercase leading-6 text-gray-700 dark:text-gray-400">
               <time dateTime={date}>{formatDate(date)}</time>
             </dd>
           </div>
@@ -21,7 +21,7 @@ export default function PostHeader({ title, summary, tags, date, contentType }) 
 
       <HeaderTagList tags={tags} />
 
-      <Badge text={contentType.join(', ')} />
+      {contentType ? <Badge text={contentType} /> : null}
 
       {/* <div className="m-4">
         <p className="italic">{summary}</p>
