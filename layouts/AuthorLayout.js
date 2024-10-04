ed70df86a3fe99a998171503d764490d2533e526
Darian Nocera <darnocer@gmail.com>
2024-10-04 08:59:17 -0600

    enable author bio

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -3,7 +3,7 @@ import Image from '@/components/Image'
 import { PageSEO } from '@/components/SEO'
 
 export default function AuthorLayout({ children, frontMatter }) {
-  const { name, avatar, occupation, company, email, twitter, linkedin, github } = frontMatter
+  const { name, avatar, occupation, company, email, twitter, linkedin, github, bio } = frontMatter
 
   return (
     <>
@@ -15,7 +15,7 @@ export default function AuthorLayout({ children, frontMatter }) {
           </h1>
         </div>
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
-          <div className="flex flex-col items-center pt-8">
+          <div className="mt-2 flex h-full flex-col items-center rounded-md bg-gray-800 bg-opacity-80 p-4">
             <Image
               src={avatar}
               alt="avatar"
@@ -23,15 +23,16 @@ export default function AuthorLayout({ children, frontMatter }) {
               height="192px"
               className="h-48 w-48 rounded-full"
             />
-            <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
-            <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
-            <div className="text-gray-500 dark:text-gray-400">{company}</div>
-            <div className="flex space-x-3 pt-6">
+            {/* <h3 className="pt-4 pb-2 text-2xl font-medium leading-8 tracking-tight">{name}</h3> */}
+            {/* <div className="text-gray-500 dark:text-gray-400">{occupation}</div> */}
+            {/* <div className="text-gray-500 dark:text-gray-400">{company}</div> */}
+            <div className="flex space-x-3 pt-2">
               <SocialIcon kind="mail" href={`mailto:${email}`} />
               <SocialIcon kind="github" href={github} />
               <SocialIcon kind="linkedin" href={linkedin} />
               <SocialIcon kind="twitter" href={twitter} />
             </div>
+            <div className="pt-4 text-center text-gray-500 dark:text-gray-300">{bio}</div>
           </div>
           <div className="prose max-w-none pt-8 pb-8 dark:prose-dark xl:col-span-2">{children}</div>
         </div>
