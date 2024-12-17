9c0ce3cf7d988e4fcc1c7fa148062e4001d6ae43
Darian Nocera <darnocer@gmail.com>
2024-12-17 08:28:59 -0700

    newsletter form styling

--- components/NewsletterForm.js
+++ components/NewsletterForm.js
@@ -27,7 +27,7 @@ const NewsletterForm = ({ title = 'Stay in Touch', description }) => {
     const { error } = await res.json()
     if (error) {
       setError(true)
-      setMessage('Your e-mail address is invalid or you are already subscribed!')
+      setMessage('Oops. Your e-mail address is invalid or you are already subscribed!')
       return
     }
 
@@ -38,18 +38,18 @@ const NewsletterForm = ({ title = 'Stay in Touch', description }) => {
   }
 
   return (
-    <SectionContainer>
-      <div className="border-t-4 border-gray-200 py-6 dark:border-gray-700">
-        <div className="justify-content flex flex-col items-center">
-          <div className="pb-1 text-lg font-semibold text-gray-800 dark:text-gray-100">{title}</div>
-          <p className="mt-2 mb-4 text-xs text-gray-400">{description}</p>
+    <SectionContainer padding="small">
+      <div className="border-t-2 border-gray-200 py-6 dark:border-gray-700">
+        <div className="justify-content flex flex-col items-start">
+          <h3 className="text-md pb-1 font-semibold text-gray-800 dark:text-gray-200">{title}</h3>
+          <p className="mt-2 mb-4 text-xs font-medium text-gray-400">{description}</p>
           <form className="flex max-w-md flex-col sm:flex-row" onSubmit={subscribe}>
             <div>
               <label htmlFor="email-input">
                 <span className="sr-only">Email address</span>
                 <input
                   autoComplete="email"
-                  className="w-72 rounded-md px-4 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-primary-600 dark:bg-black"
+                  className="w-72 rounded-md px-4 text-sm font-medium focus:border-transparent focus:outline-none focus:ring-2 focus:ring-primary-600 dark:bg-black"
                   id="email-input"
                   name="email"
                   placeholder={subscribed ? "You're subscribed !  🎉" : 'Enter your email'}
@@ -62,7 +62,7 @@ const NewsletterForm = ({ title = 'Stay in Touch', description }) => {
             </div>
             <div className="mt-2 flex w-full rounded-md shadow-sm sm:mt-0 sm:ml-3">
               <button
-                className={`w-full rounded-md bg-primary-500 py-2 px-4 font-medium text-white transition-all duration-300 ease-in sm:py-0 ${
+                className={`w-full rounded-md bg-primary-500 py-4 px-6 text-sm font-semibold uppercase leading-none text-white transition-all duration-300 ease-in sm:py-0 ${
                   subscribed ? 'cursor-default' : 'hover:bg-primary-700 dark:hover:bg-primary-400'
                 } focus:outline-none focus:ring-2 focus:ring-primary-600 focus:ring-offset-2 dark:ring-offset-black`}
                 type="submit"
@@ -73,7 +73,7 @@ const NewsletterForm = ({ title = 'Stay in Touch', description }) => {
             </div>
           </form>
           {error && (
-            <div className="w-72 pt-2 text-sm text-red-500 dark:text-red-400 sm:w-96">
+            <div className="w-72 pt-2 text-sm text-accent-500 dark:text-accent-400 sm:w-96">
               {message}
             </div>
           )}
