2a7372537490948d74c3d9db44e2459ae8a2e36e
Darian Nocera <darnocer@gmail.com>
2025-01-12 15:57:52 -0700

    add card grid component and data

---
+++ components/CardGrid.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import Image from 'next/image'
+import cardData from '@/data/cardData'
+import Link from '@/components/Link'
+import Heading from '@/components/Heading'
+import SectionContainer from './SectionContainer'
+
+const CardGrid = ({ heading }) => {
+  return (
+    <SectionContainer padding="large" container="medium">
+      <Heading level="h2" text={heading} />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 py-4 sm:py-6 auto-rows-fr">
+        {cardData.map(({ title, link, icon, status }, index) => {
+          const isDisabled = status === 'disabled'
+
+          return (
+            <Link href={link}>
+            <div
+              key={index}
+              className={`relative flex flex-col justify-between rounded-lg border px-4 py-6 shadow-lg transition-all ease duration-200 dark:bg-black bg-black bg-opacity-80 border-gray-800 hover:border-black dark:border-gray-300/20 dark:hover:border-gray-400/40 cursor-pointer h-full
+                ${isDisabled ? 'cursor-not-allowed' : 'hover:border-gray-600/40 hover:shadow-none hover:bg-gray-700 dark:hover:bg-gray-850'}`}
+            >
+              {isDisabled && (
+                <div className="absolute inset-0 flex items-center justify-center rounded-lg bg-black bg-opacity-50 text-white text-lg font-bold opacity-0 transition-opacity hover:opacity-100">
+                  Coming Soon
+                </div>
+              )}
+              <div className="text-3xl mb-4">
+                {icon.startsWith('/') ? (
+                  <Image src={icon} alt={title} width={32} height={32} />
+                ) : (
+                  <span>{icon}</span>
+                )}
+              </div>
+              <h3 className="text-lg font-sans font-bold text-gray-100 dark:text-gray-100">{title}</h3>
+            </div>
+            </Link>
+          )
+        })}
+      </div>
+    </SectionContainer>
+  )
+}
+
+export default CardGrid
---
+++ data/cardData.js
@@ -0,0 +1,8 @@
+const cardData = [
+  { title: 'Project Leadership', link: '/work/management', icon: '💻' },
+  { title: 'Transformational Wellness', link: '/work/spiritual', icon: '✨' },
+  { title: 'Microdose.wiki', link: 'https://microdose.wiki', icon: '/static/images/microdose.svg' },
+  { title: 'Soul Science', link: 'https://soulsciencehq.com', icon: '/static/images/soul-science.svg', status: 'disabled'},
+]
+
+export default cardData
