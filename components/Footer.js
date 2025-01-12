687c871caca86be2973017ec8f21f0d4556c5043
Darian Nocera <darnocer@gmail.com>
2025-01-12 15:59:34 -0700

    implement footer nav links data

--- components/Footer.js
+++ components/Footer.js
@@ -1,30 +1,35 @@
 import Link from '@/components/Link'
 import siteMetadata from '@/data/siteMetadata'
+import footerNavLinks from '@/data/footerNavLinks'
 import SocialIcon from '@/components/icons/social-icons'
 
 export default function Footer() {
   return (
-    <footer>
+    <footer className="mt-auto">
       <div className="mt-12 flex flex-col items-center font-medium">
-        {/* <div className="mb-3 flex space-x-4">
-          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size="6" />
-          <SocialIcon kind="github" href={siteMetadata.github} size="6" />
-          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="6" />
-        </div> */}
-        <Link
-          className="text-xxs uppercase text-gray-500 underline dark:text-gray-400"
-          href="/colophon"
-        >
-          Colophon
-        </Link>
-        <div className="mt-4 mb-2 flex space-x-2 text-xxs text-gray-500 dark:text-gray-400">
+        <div className="mb-3 flex space-x-4">
+        {footerNavLinks.map((link, index) => (
+            <span key={link.title} className="flex items-center">
+              <Link
+                className="text-xxs uppercase text-gray-500 underline dark:text-gray-400"
+                href={link.href}
+              >
+                {link.title}
+              </Link>
+              {index < footerNavLinks.length - 1 && (
+                <span className="ml-2  text-gray-500 dark:text-gray-400">|</span>
+              )}
+            </span>
+          ))}
+        </div>
+        <div className="mt-4 mb-2 flex flex-col items-center text-xxs text-gray-500 dark:text-gray-400">
           <p className="text-xxs">
             Made with 👽 by{' '}
             <a className="underline" href={siteMetadata.website} target="_blank" rel="noreferrer">
               darian.
             </a>
           </p>
-          <div>{`© ${new Date().getFullYear()}`}</div>
+          <p>{`© ${new Date().getFullYear()} rootedvision`}</p>
         </div>
       </div>
     </footer>
---
+++ data/footerNavLinks.js
@@ -0,0 +1,8 @@
+const footerNavLinks = [
+{ href: '/colophon', title: 'Colophon' }, 
+{ href: 'https://linkedin.com/in/darian-nocera', title: 'LinkedIn' }, 
+{ href: 'https://drive.google.com/file/d/1iTvpI1tIrhsLtupMHxhMja_BJkFOAb55/view?usp=drive_link', title: 'Resume' }, 
+{ href: 'mailto:d@rootedvision.co', title: 'Email' }, 
+]
+
+export default footerNavLinks
