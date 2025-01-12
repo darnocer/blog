a36729354a8c9d3405ba1039268b02b70ec1aab5
Darian Nocera <darnocer@gmail.com>
2025-01-12 16:00:34 -0700

    update logo file reference in logo component

--- components/Logo.js
+++ components/Logo.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import Link from 'next/link'
-import LogoDark from '@/data/logo.svg'
-import LogoLight from '@/data/logo-light.svg'
 import siteMetadata from '@/data/siteMetadata'
 
 const Logo = () => {
   return (
-    <Link href="/" aria-label={siteMetadata.headerTitle} passHref>
-      <div style={{ width: '75px' }} className="hover:cursor-pointer">
+    <Link style={{ width: '50px' }} href="/" aria-label={siteMetadata.headerTitle} passHref>
+      <div className="hover:cursor-pointer">
         <div className="block dark:hidden">
-          <LogoLight style={{ width: '100%', height: 'auto', display: 'block' }} />
+          <img
+            src="/static/images/logos/logo-light.svg"
+            alt="Logo Light"
+            style={{ width: '100%', height: 'auto', display: 'block' }}
+          />
         </div>
         <div className="hidden dark:block">
-          <LogoDark style={{ width: '100%', height: 'auto', display: 'block' }} />
+          <img
+            src="/static/images/logos/logo.svg"
+            alt="Logo Dark"
+            style={{ width: '100%', height: 'auto', display: 'block' }}
+          />
         </div>
       </div>
     </Link>
