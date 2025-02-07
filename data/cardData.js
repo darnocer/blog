acfc02e9d51c10bc0e9bd1d10481911828a80b78
Darian Nocera <darnocer@gmail.com>
2025-02-07 10:53:38 -0700

    update card data to include posts

--- data/cardData.js
+++ data/cardData.js
@@ -2,16 +2,16 @@ import LaptopIcon from '@/components/icons/LaptopIcon'
 import SparklesIcon from '@/components/icons/SparklesIcon'
 import MicrodoseIcon from '@/components/icons/MicrodoseIcon'
 import SoulScienceIcon from '@/components/icons/SoulScienceIcon'
+import PencilIcon from '@/components/icons/PencilIcon'
 
 const cardData = [
   { title: 'Project Leadership', link: '/work/management', icon: <LaptopIcon /> },
   { title: 'Transformational Wellness', link: '/work/spiritual', icon: <SparklesIcon /> },
   { title: 'Microdose.wiki', link: 'https://microdose.wiki', icon: <MicrodoseIcon /> },
   {
-    title: 'Soul Science',
-    link: 'https://soulsciencehq.com',
-    icon: <SoulScienceIcon />,
-    status: 'disabled',
+    title: 'Writing',
+    link: '/posts',
+    icon: <PencilIcon />,
   },
 ]
 
