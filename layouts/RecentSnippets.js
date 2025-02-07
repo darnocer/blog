048001e2985514b81e4cc392938742364c4c4167
Darian Nocera <darnocer@gmail.com>
2025-02-07 10:55:04 -0700

    update recent snippets layout

--- layouts/RecentSnippets.js
+++ layouts/RecentSnippets.js
@@ -5,9 +5,10 @@ import LinkArrow from '@/components/LinkArrow'
 import Link from '@/components/Link'
 import Tag from '@/components/Tag'
 import Heading from '@/components/Heading'
+import ChevronRight from '@/components/icons/ChevronRightIcon'
 import SectionContainer from '@/components/SectionContainer'
 import Badge from '@/components/Badge'
-import { MDXComponents } from '@/components/MDXComponents' // ✅ Import your MDX components
+import { MDXComponents } from '@/components/MDXComponents'
 
 const MAX_SNIPPETS = 5
 const MAX_PREVIEW_LENGTH = 1000
@@ -32,9 +33,9 @@ export default function RecentSnippets({ heading }) {
     <>
       <Heading text={heading} />
       <div className='space-y-6'>
-        <ul className='space-y-6'>
+        <ul className='space-y-6 divide-y divide-gray-200 dark:divide-gray-700'>
           {snippets.length === 0 && 'No snippets found.'}
-          {snippets.map(({ frontMatter, previewText }) => {
+          {snippets.map(({ frontMatter, mdxSource }) => {
             const { slug, date, title, tags, content_type } = frontMatter
             return (
               <li key={slug} className='group no-arrow py-2'>
@@ -48,8 +49,8 @@ export default function RecentSnippets({ heading }) {
                         {formatDateShort(date)}
                       </time>
                     )}{' '}
-                    <span className='text-gray-700 dark:text-gray-400'>|</span>
-                    {content_type && <Badge text={content_type} />}
+                    {/* <span className='text-gray-700 dark:text-gray-400'>|</span>
+                    {content_type && <Badge text={content_type} />} */}
                     <span className='text-gray-700 dark:text-gray-400'>|</span>
                     {tags && (
                       <div className='flex items-baseline gap-x-2'>
@@ -60,11 +61,17 @@ export default function RecentSnippets({ heading }) {
                     )}
                   </div>
                   <Link href={`/${slug}`} className='text-gray-900 dark:text-gray-200'>
-                    <h3 className='text-2xl font-bold tracking-tight hover:underline'>{title}</h3>
+                    <h3 className='text-3xl font-bold tracking-tight hover:underline'>{title}</h3>
                   </Link>
 
-                  <div className='mdx-preview text-sm text-gray-800 dark:text-gray-200 leading-relaxed'>
-                    {previewText}
+                  <div className='mdx-preview prose max-w-full'>
+                    <MDXRemote {...mdxSource} />
+                    <Link
+                      href={`/${slug}`}
+                      className='inline-flex justify-center items-center font-semibold font-sans tracking-tight hover:underline'
+                    >
+                      Read More <ChevronRight className='mb-1' />
+                    </Link>
                   </div>
                 </article>
               </li>
