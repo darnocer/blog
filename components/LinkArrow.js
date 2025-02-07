e8c92b33c6c4b2822b6c5d269b430b67824191d7
Darian Nocera <darnocer@gmail.com>
2025-02-07 10:53:18 -0700

    move chevron svgs to components

--- components/LinkArrow.js
+++ components/LinkArrow.js
@@ -1,46 +1,18 @@
 import React, { Component } from 'react'
 import Link from '@/components/Link'
+import ChevronLeft from '@/components/icons/ChevronLeftIcon'
+import ChevronRight from '@/components/icons/ChevronRightIcon'
 
 export default function LinkArrow({ text, direction, href }) {
   return (
     <Link href={href}>
-      <div className="my-4 flex justify-start text-base font-semibold leading-6">
-        <span className="flex items-center text-primary-500 duration-300 ease-in hover:text-primary-600 hover:underline dark:hover:text-primary-400">
-          {direction === 'left' && (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="18"
-              height="18"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="lucide lucide-chevron-left"
-            >
-              <path d="m15 18-6-6 6-6" />
-            </svg>
-          )}
+      <div className='my-4 flex justify-start text-base font-semibold leading-6'>
+        <span className='flex items-center text-primary-500 duration-300 ease-in hover:text-primary-600 hover:underline dark:hover:text-primary-400'>
+          {direction === 'left' && <ChevronLeft className='mr-1' />}
 
           {text}
 
-          {direction === 'right' && (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="18"
-              height="18"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="lucide lucide-chevron-right"
-            >
-              <path d="m9 18 6-6-6-6" />
-            </svg>
-          )}
+          {direction === 'right' && <ChevronRight className='ml-1' />}
         </span>
       </div>
     </Link>
