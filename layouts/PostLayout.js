27cdac3793fe6efddf18b3dc0f2c4bd0b056af5f
Darian Nocera <darnocer@gmail.com>
2025-02-10 09:23:26 -0700

    updates to PostLayout

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -7,6 +7,11 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import Comments from '@/components/comments'
 import ScrollTopAndComment from '@/components/ScrollTopAndComment'
+import ScrollIndicator from '@/components/ScrollIndicator'
+import PostHeaderSimple from '@/components/PostHeaderSimple'
+import ChevronLeft from '@/components/icons/ChevronLeftIcon'
+import ChevronRight from '@/components/icons/ChevronRightIcon'
+import LinkArrow from '@/components/LinkArrow'
 
 const editUrl = (fileName) => `${siteMetadata.siteRepo}/blob/master/data/blog/${fileName}`
 const discussUrl = (slug) =>
@@ -15,30 +20,17 @@ const discussUrl = (slug) =>
 const postDateTemplate = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
 
 export default function PostLayout({ frontMatter, authorDetails, next, prev, children }) {
-  const { slug, fileName, date, title, images, tags } = frontMatter
+  const { slug, fileName, date, title, images, tags, summary, content_type } = frontMatter
 
   return (
     <SectionContainer padding='medium' container='small'>
       <BlogSEO url={`${siteMetadata.siteUrl}/blog/${slug}`} authorDetails={authorDetails} {...frontMatter} />
       <ScrollTopAndComment />
+      <ScrollIndicator direction='left' />
       <article>
         <div className='xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700'>
-          <header className='pt-6 xl:pb-6'>
-            <div className='space-y-1 text-center'>
-              <dl className='space-y-10'>
-                <div>
-                  <dt className='sr-only'>Published on</dt>
-                  <dd className='text-base font-medium  leading-6 text-gray-500 dark:text-gray-400'>
-                    <time dateTime={date}>
-                      {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
-                    </time>
-                  </dd>
-                </div>
-              </dl>
-              <div>
-                <PageTitle>{title}</PageTitle>
-              </div>
-            </div>
+          <header>
+            <PostHeaderSimple title={title} summary={summary} tags={tags} date={date} contentType={content_type} />
           </header>
           <div
             className='divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6 xl:divide-y-0'
@@ -80,21 +72,21 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
               </dd>
             </dl>
             <div className='divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0'>
-              <div className='prose max-w-none pt-10 pb-8 dark:prose-dark'>{children}</div>
-              <div className='pt-6 pb-6 text-sm text-gray-700 dark:text-gray-300'>
+              <div className='prose max-w-none pt-10 pb-8 dark:prose-dark font-serif'>{children}</div>
+              {/* <div className='pt-6 pb-6 text-sm text-gray-700 dark:text-gray-300'>
                 <Link href={discussUrl(slug)} rel='nofollow'>
                   {'Discuss on Twitter'}
                 </Link>
                 {` • `}
                 <Link href={editUrl(fileName)}>{'View on GitHub'}</Link>
               </div>
-              <Comments frontMatter={frontMatter} />
+              <Comments frontMatter={frontMatter} /> */}
             </div>
             <footer>
               <div className='divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700 xl:col-start-1 xl:row-start-2 xl:divide-y'>
                 {tags && (
                   <div className='py-4 xl:py-8'>
-                    <h2 className='text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400'>Tags</h2>
+                    <h2 className='text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400'>Tags:</h2>
                     <div className='mt-2 flex flex-wrap gap-y-2'>
                       {tags.map((tag) => (
                         <Tag key={tag} text={tag} />
@@ -107,10 +99,10 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
                     {prev && (
                       <div>
                         <h2 className='text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400'>
-                          Previous Article
+                          Previous Article:
                         </h2>
-                        <div className='mt-2 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400'>
-                          <Link href={`/blog/${prev.slug}`}>{prev.title}</Link>
+                        <div className='mt-2 font-semibold text-primary-500 hover:text-primary-600 dark:hover:text-primary-400'>
+                          <LinkArrow direction='right' href={`/${prev.slug}`} text={prev.title} />
                         </div>
                       </div>
                     )}
@@ -120,7 +112,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
                           Next Article
                         </h2>
                         <div className='mt-2 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400'>
-                          <Link href={`/blog/${next.slug}`}>{next.title}</Link>
+                          <LinkArrow direction='left' href={`/blog/${next.slug}`} text={next.title} />
                         </div>
                       </div>
                     )}
