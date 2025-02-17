cc9a37b9f1e280f9eda2f8e44edd02d1edc76972
Darian Nocera <darnocer@gmail.com>
2025-02-17 13:47:32 -0700

    resolve icons errors

--- components/icons/components/MeditationIcon.js
+++ components/icons/components/MeditationIcon.js
@@ -7,10 +7,10 @@ const MeditationIcon = () => {
       viewBox='0 0 24 24'
       fill='none'
       stroke='currentColor'
-      stroke-width='2'
-      stroke-linecap='round'
-      stroke-linejoin='round'
-      class='lucide lucide-brain'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className='lucide lucide-brain'
     >
       <path d='M12 5a3 3 0 1 0-5.997.125 4 4 0 0 0-2.526 5.77 4 4 0 0 0 .556 6.588A4 4 0 1 0 12 18Z' />
       <path d='M12 5a3 3 0 1 1 5.997.125 4 4 0 0 1 2.526 5.77 4 4 0 0 1-.556 6.588A4 4 0 1 1 12 18Z' />
