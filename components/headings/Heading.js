bbcf78cf8a5aecce66c55b8f14ecb345dda3b6e0
Darian Nocera <darnocer@gmail.com>
2025-02-18 09:05:43 -0700

    enable subtitles for h1 headings

--- components/headings/Heading.js
+++ components/headings/Heading.js
@@ -1,15 +1,20 @@
 import React from 'react'
 
-export default function Heading({ text, level = 'h2', border = 'border', color = 'gray-200' }) {
+export default function Heading({ text, level = 'h2', border = 'border', color = 'gray-200', subtitle }) {
   const borderClasses = border === 'border' ? 'mb-8 border-b-2 border-gray-300 py-2 dark:border-gray-500 font-sans' : ''
 
   const HeadingLevels = {
     h1: ({ text }) => (
-      <h1
-        className={`text-5xl font-sans font-bold leading-none tracking-tighter text-gray-800 dark:text-gray-200 ${borderClasses}`}
-      >
-        {text}
-      </h1>
+      <>
+        {subtitle ? (
+          <span className='font-sans font-bold text-2xl text-gray-700 dark:text-gray-300 mb-4'>{subtitle}</span>
+        ) : null}
+        <h1
+          className={`text-5xl font-sans font-bold leading-none tracking-tighter text-gray-800 dark:text-gray-200 ${borderClasses}`}
+        >
+          {text}
+        </h1>
+      </>
     ),
     h2: ({ text }) => (
       <h2
