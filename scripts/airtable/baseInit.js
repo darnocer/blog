aada3b19a609e0faf4fe49b3f60a23dc68def2b3
Darian Nocera <darnocer@gmail.com>
2025-02-19 08:36:22 -0700

    remove airtable sync

--- scripts/airtable/baseInit.js
+++
@@ -1,7 +0,0 @@
-const Airtable = require('airtable')
-require('dotenv').config()
-
-// Initialize Airtable base
-const base = new Airtable({ apiKey: process.env.AIRTABLE_TOKEN }).base(process.env.AIRTABLE_BASE_ID)
-
-module.exports = base
