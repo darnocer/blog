404f4168fed9fe4be15f12bb8875bbd5e6021c12
Darian Nocera <darnocer@gmail.com>
2025-02-25 07:52:41 -0700

    update homeCardData

--- data/cards/homeCardData.js
+++ data/cards/homeCardData.js
@@ -2,15 +2,14 @@ import LaptopIcon from '@/components/icons/components/LaptopIcon'
 import SparklesIcon from '@/components/icons/components/SparklesIcon'
 import MicrodoseIcon from '@/components/icons/components/MicrodoseIcon'
 import PencilIcon from '@/components/icons/components/PencilIcon'
+import PsychedelicsIcon from '@/components/icons/components/PsychedelicsIcon'
+import MeditationIcon from '@/components/icons/components/MeditationIcon'
+import SoulScienceIcon from '@/components/icons/components/SoulScienceIcon'
 
 const homeCardData = [
-  { title: 'Transformational Wellness', link: '/work/spiritual', icon: <SparklesIcon /> },
-  { title: 'Project Leadership', link: '/work/management', icon: <LaptopIcon /> },
-  {
-    title: 'Writing',
-    link: '/work/writing',
-    icon: <PencilIcon />,
-  },
+  { title: 'Psychedelic Facilitation', link: '/work/psychedelics', icon: <PsychedelicsIcon /> },
+  { title: 'Meditation & Mindfulness', link: '/work/meditation', icon: <MeditationIcon /> },
+  { title: 'Soul Signals Newsletter', link: 'https://soul-science.com', icon: <SoulScienceIcon /> },
   { title: 'Microdose.wiki', link: 'https://microdose.wiki', icon: <MicrodoseIcon /> },
 ]
 
