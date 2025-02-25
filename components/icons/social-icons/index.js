c0940214d9731db51c6728a618f7f34199caede1
Darian Nocera <darnocer@gmail.com>
2025-02-25 07:52:00 -0700

    Implement about page: - Add medium/website icons - update social icon rendering - update AuthorLayout - add about.mdx content

--- components/icons/social-icons/index.js
+++ components/icons/social-icons/index.js
@@ -4,6 +4,8 @@ import Facebook from './facebook.svg'
 import Youtube from './youtube.svg'
 import Linkedin from './linkedin.svg'
 import Twitter from './twitter.svg'
+import Medium from './medium.svg'
+import Website from './website.svg'
 
 // Icons taken from: https://simpleicons.org/
 
@@ -14,24 +16,26 @@ const components = {
   youtube: Youtube,
   linkedin: Linkedin,
   twitter: Twitter,
+  medium: Medium,
+  website: Website,
 }
 
 const SocialIcon = ({ kind, href, size = 8 }) => {
-  if (!href || (kind === 'mail' && !/^mailto:\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/.test(href)))
-    return null
+  if (!href || (kind === 'mail' && !/^mailto:\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/.test(href))) return null
 
   const SocialSvg = components[kind]
 
   return (
     <a
-      className="text-sm text-gray-500 transition hover:text-gray-600"
-      target="_blank"
-      rel="noopener noreferrer"
+      className='text-sm text-gray-500 transition hover:text-gray-600'
+      target='_blank'
+      rel='noopener noreferrer'
       href={href}
     >
-      <span className="sr-only">{kind}</span>
+      <span className='sr-only'>{kind}</span>
       <SocialSvg
-        className={`h-6 fill-current text-gray-700 duration-200 ease-in hover:text-secondary-500 dark:text-gray-200 dark:hover:text-primary-400`}
+        className={`h-6 text-gray-700 duration-200 ease-in hover:text-tertiary-600 dark:text-gray-200 dark:hover:text-tertiary-500`}
+        fill={kind === 'website' ? 'none' : 'currentColor'} // Ensure website icon uses stroke only
       />
     </a>
   )
