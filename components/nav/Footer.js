09b97d4ef11d8acf757723db88d6e57ca3384273
Darian Nocera <darnocer@gmail.com>
2025-02-25 07:52:24 -0700

    Footer: update Footer styling and footer links

--- components/nav/Footer.js
+++ components/nav/Footer.js
@@ -2,22 +2,28 @@ import Link from '@/components/links/Link'
 import siteMetadata from '@/data/siteMetadata'
 import footerNavLinks from '@/data/nav/footerNavLinks'
 import SocialIcon from '@/components/icons/social-icons'
+import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className='mt-auto'>
+    <footer className='mt-auto divide-y border-t-1 border-gray-500/50 bg-white dark:bg-black'>
       <div className='mt-12 flex flex-col items-center font-medium'>
-        <div className='mb-3 flex space-x-4'>
+        <nav className='mb-3 flex items-center space-x-4'>
           {footerNavLinks.map((link, index) => (
-            <span key={link.title} className='flex items-center'>
-              <Link className='text-xxs uppercase text-gray-600 underline dark:text-gray-400' href={link.href}>
+            <React.Fragment key={link.href}>
+              <Link
+                href={link.href}
+                className='text-xs font-semibold uppercase text-gray-400 transition-all duration-200 ease-in hover:text-gray-500 hover:underline dark:hover:text-gray-300'
+              >
                 {link.title}
               </Link>
-              {index < footerNavLinks.length - 1 && <span className='ml-2  text-gray-500 dark:text-gray-400'>|</span>}
-            </span>
+              {index < footerNavLinks.length - 1 && ( // Only add the pipe between links, not at the end
+                <span className='mx-2 text-gray-500 dark:text-gray-400'>|</span>
+              )}
+            </React.Fragment>
           ))}
-        </div>
-        <div className='mt-4 mb-2 flex flex-col items-center text-xxs text-gray-500 dark:text-gray-400'>
+        </nav>
+        <div className='mb-2 mt-4 flex flex-col items-center text-xxs text-gray-500 dark:text-gray-400'>
           <p className='text-xxs'>
             Made with 👽 by{' '}
             <a className='underline' href={siteMetadata.website} target='_blank' rel='noreferrer'>
--- data/nav/footerNavLinks.js
+++ data/nav/footerNavLinks.js
@@ -1,12 +1,14 @@
 const footerNavLinks = [
   { href: '/colophon', title: 'Colophon' },
-  { href: '/sitemap', title: 'Sitemap' },
+  // { href: '/sitemap', title: 'Sitemap' },
   { href: 'https://linkedin.com/in/darian-nocera', title: 'LinkedIn' },
+  { href: 'https://medium.com/@soul-science', title: 'Medium' },
+  { href: 'mailto:darian@soul-science.com?Subject=Website Contact', title: 'Email' },
+  { href: 'https://calendly.com/soul-science/30', title: 'Schedule' },
   {
     href: 'https://drive.google.com/file/d/1iTvpI1tIrhsLtupMHxhMja_BJkFOAb55/view?usp=drive_link',
     title: 'Resume',
   },
-  { href: 'mailto:d@rootedvision.co?Subject=Website Contact', title: 'Email' },
 ]
 
 export default footerNavLinks
