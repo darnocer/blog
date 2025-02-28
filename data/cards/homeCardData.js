104ef72dc738689773261cffb7a3120f7bdb8319
Darian Nocera <darnocer@gmail.com>
2025-02-28 10:49:47 -0700

    update homecard data

--- data/cards/homeCardData.js
+++ data/cards/homeCardData.js
@@ -7,10 +7,10 @@ import MeditationIcon from '@/components/icons/components/MeditationIcon'
 import SoulScienceIcon from '@/components/icons/components/SoulScienceIcon'
 
 const homeCardData = [
-  { title: 'Psychedelic Facilitation', link: '/work/psychedelics', icon: <PsychedelicsIcon /> },
-  { title: 'Meditation & Mindfulness', link: '/work/meditation', icon: <MeditationIcon /> },
-  { title: 'Soul Signals Newsletter', link: 'https://soul-science.com', icon: <SoulScienceIcon /> },
+  { title: 'Transformational Wellness', link: '/transformational-wellness', icon: <SparklesIcon /> },
+  { title: 'Soul Signals Newsletter', link: 'https://soul-science.com', icon: <SoulScienceIcon />, status: 'disabled' },
   { title: 'Microdose.wiki', link: 'https://microdose.wiki', icon: <MicrodoseIcon /> },
+  { title: 'Writing', link: '/writing', icon: <PencilIcon /> },
 ]
 
 export default homeCardData
