ddde7d49c9385329fb98807bc5c97ed6319957ae
Darian Nocera <darnocer@gmail.com>
2025-06-08 16:44:23 -0600

    update icons in linkBlocks

---
+++ data/cards/linkBlocks.js
@@ -0,0 +1,30 @@
+import { Mail, Globe, Linkedin, Twitter, Instagram, BookOpen, CalendarPlus } from 'lucide-react'
+
+import SoulScienceIcon from '@/components/icons/components/SoulScienceIcon'
+import MicrodoseIcon from '@/components/icons/components/MicrodoseIcon'
+import DarianDigitalIcon from '@/components/icons/components/DarianDigitalIcon'
+
+const linkBlocks = [
+  {
+    title: 'Soul Science',
+    url: 'https://www.soulsciencehq.com',
+    icon: SoulScienceIcon,
+  },
+  {
+    title: 'Microdose.Wiki',
+    url: 'https://microdose.wiki',
+    icon: MicrodoseIcon,
+  },
+  {
+    title: 'darian.digital',
+    url: 'https://drive.google.com/file/d/1K15gxW-LqSCN0RbsmkotsjSpPwyvikh_/view?usp=sharing',
+    icon: DarianDigitalIcon,
+  },
+  {
+    title: 'Schedule 30 minutes',
+    url: 'https://calendly.com/soulsciencehq/30',
+    icon: CalendarPlus,
+  },
+]
+
+export default linkBlocks
